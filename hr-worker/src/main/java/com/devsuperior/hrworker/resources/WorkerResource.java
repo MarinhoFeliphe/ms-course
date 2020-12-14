aab250c010a77ee7cf1aae93df7f2456459670a3
feliphe.marinho <feliphemarinho18@gmail.com>
2020-12-14 18:06:49 -0300

    feat: configure eureka clients

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -4,7 +4,10 @@ import static org.springframework.http.ResponseEntity.ok;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,9 +20,12 @@ import com.devsuperior.hrworker.repositories.WorkerRepository;
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
-
+	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+	
 	@Autowired
 	private WorkerRepository repository;
+	@Autowired
+	private Environment env;
 	
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll() {
@@ -29,6 +35,9 @@ public class WorkerResource {
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
+		
+		logger.info("Port = " + env.getProperty("local.server.port"));
+		
 		Worker worker = repository.findById(id).get();
 		return ok(worker);
 	}
