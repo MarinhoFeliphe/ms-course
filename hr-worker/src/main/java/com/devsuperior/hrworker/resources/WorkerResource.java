9057cbf23e72d4aa855dec61ac0cc460584f7f62
feliphe.marinho <feliphemarinho18@gmail.com>
2020-12-21 17:08:24 -0300

    feat: get test configurations

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,10 +28,21 @@ public class WorkerResource {
 	@Autowired
 	private Environment env;
 	
+	@Value("${test.config}")
+	private String testConfig;
+	
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll() {
 		List<Worker> workers = repository.findAll();
 		return ok(workers);
+	}
+	
+	@GetMapping("/configs")
+	public ResponseEntity<Void> findConfigs() {
+		
+		logger.info("Config = " + testConfig);
+		
+		return ResponseEntity.noContent().build();
 	}
 	
 	@GetMapping("/{id}")
